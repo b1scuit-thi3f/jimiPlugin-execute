60bc48866370539dcf14ee860d58d16236120a63
Patrick Lynch <patrick.lynch@unipart.com>
2021-02-15 10:42:09 +0000

    upgraded webui and changed inputs

--- models/action.py
+++ models/action.py
@@ -1,11 +1,34 @@
-from core.models import action
+from core.models import action, webui
+from core import helpers
 import subprocess
 
 class _execute(action._action):
-	options = dict()
+	program = str()
+	arguments = str()
 	timeout = int()
 
+	class _properties(webui._properties):
+        def generate(self,classObject):
+            formData = []
+            formData.append({"type" : "break", "start" : True, "schemaitem": "Execute Options"})
+            formData.append({"type" : "input", "schemaitem" : "program", "textbox" : classObject.program, "tooltip" : "The program to execute (e.g. python3)"})
+            formData.append({"type" : "input", "schemaitem" : "arguments", "textbox" : classObject.arguments, "tooltip" : "Arguments to pass to the program"})
+            formData.append({"type" : "input", "schemaitem" : "timeout", "textbox" : classObject.timeout, "tooltip" : "How long to let the process run before killing it"})
+            formData.append({"type" : "break", "start" : False, "schemaitem": "Execute Options"})
+            formData.append({"type" : "break", "start" : True, "schemaitem": "Core Options"})
+            formData.append({"type" : "input", "schemaitem" : "_id", "textbox" : classObject._id})
+            formData.append({"type" : "input", "schemaitem" : "name", "textbox" : classObject.name})
+            formData.append({"type" : "checkbox", "schemaitem" : "enabled", "checked" : classObject.enabled})
+            formData.append({"type" : "json-input", "schemaitem" : "varDefinitions", "textbox" : classObject.varDefinitions})
+            formData.append({"type" : "input", "schemaitem" : "logicString", "textbox" : classObject.logicString})
+            formData.append({"type" : "checkbox", "schemaitem" : "log", "checked" : classObject.log})
+            formData.append({"type" : "input", "schemaitem" : "comment", "textbox" : classObject.comment})
+            return formData
+
 	def run(self,data,persistentData,actionResult):
+		program = helpers.evalString(self.program,{"data" : data})
+		arguments = helpers.evalString(self.arguments,{"data" : data})
+
 		actionResult["result"] = False
 		actionResult["rc"] = 1
 		if "program" in self.options:
